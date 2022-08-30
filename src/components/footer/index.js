4925724b64dc049996e596f36424f1376cad78c0
Pedro Wanderley <wanderley.pedro@outlook.com>
2022-08-30 15:52:56 -0300

    feat: footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+const Footer = () => {
+  return (
+    <Container>
+      <p>AdoptPets - since 2022</p>
+    </Container>
+  );
+};
+
+export default Footer;
---
+++ src/components/footer/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 50px;
+  background-color: #005bea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    color: #fff;
+  }
+`;
